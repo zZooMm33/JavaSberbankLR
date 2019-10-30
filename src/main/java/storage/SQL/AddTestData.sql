5cf360e99239be6ec793e62088c76d578d412bfe
zzoomm <mmmoonnn@mail.ru>
2019-10-31 00:59:53 +0300

    + hotel +
    Signed-off-by: zzoomm <mmmoonnn@mail.ru>

--- src/main/java/storage/SQL/AddTestData.sql
+++ src/main/java/storage/SQL/AddTestData.sql
@@ -1,2 +1,6 @@
+INSERT INTO user_info (FIRST_NAME, LAST_NAME, MAIL, DATE_OF_BIRTH, SEX) VALUES ('Артем', 'Луньков', 'mmmoonnn@mail.ru', '1988-04-10', 'Мужской');
+
 INSERT INTO HOTEL (NAME, DESCRIPTION, COUNTRY, CITY, STAR, WEBSITE) VALUES ('zZooMm', 'well yes zZooMm', 'Russia', 'Bryansk', 6, 'zZooMm.ru');
 INSERT INTO HOTEL (NAME, DESCRIPTION, COUNTRY, CITY, STAR, WEBSITE) VALUES ('zZooMm 2', 'well yes zZooMm 2', 'Russia', 'Bryansk', 1, 'zZooMm2.ru');
+
+INSERT INTO HOTEL_REVIEW (ID_USER, ID_HOTEL, DATE_OF_VISIT, RATING, DESCRIPTION) VALUES (1, 1, '2019-04-10', 10, 'ТОПЧИК');
